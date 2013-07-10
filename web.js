a3cc8910fdf4f1856e1df75d948d94ae52fbe18b
Alvaro Iradier <airadier@gmail.com>
2013-07-10 10:51:24 +0200

    Another fix

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  data = fs.readFileSync('index.html');
+  var buff = fs.readFileSync('index.html');
   response.send(buf.toString());
 });
 
